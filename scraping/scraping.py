fb01d6e76e9fecbe0b2206aaf87bcbff01abd68c
000Bo <fbonnisseau.04@gmail.com>
2024-03-26 10:43:48 +0100

    structure OOP et enregistrement des données : ok

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -8,7 +8,6 @@ import os
 from bs4 import BeautifulSoup
 import requests
 
-today_date = date.today().strftime("%d/%m/%Y").replace("/", "_")
 headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
         }
@@ -35,7 +34,10 @@ class Site:
             description = self._extract_text(product, self.selectors['description'])
             url_product = self._extract_attribute(product, self.selectors['url_product'], 'href')
             url_image = self._extract_attribute(product, self.selectors['url_image'], 'src')
-
+            
+            if self.nom == 'materiel':
+                brand = brand.split(" ")[0]
+                
             data = {
                 'Nom': name,
                 'Marque': brand,
@@ -46,6 +48,7 @@ class Site:
             }
             
             self.data_list.append(data)
+        return self.data_list
 
     def _extract_text(self, element, selector):
         selected_element = element.select_one(selector)
@@ -72,14 +75,37 @@ class Site:
             # Fermer le fichier CSV
             csv_file.close()
             
-    def _write_data(self, product_name):
+    def write_data(self, product_name):
+        today_date = date.today().strftime("%d/%m/%Y").replace("/", "_")
         self.get_infos(product_name)
         if os.path.isdir(today_date):
             os.chdir(today_date)
-            self.save_data_in_csv(self, product_name)
+            self._save_data_in_csv(product_name)
             os.chdir('..')
         else:
             os.mkdir(today_date)
             os.chdir(today_date)
-            self.save_data_in_csv(self, product_name)
+            self._save_data_in_csv(product_name)
             os.chdir('..')
+
+
+if __name__ == "__main__":
+    # Define the base URL and selectors for the Materiel.net website
+    base_url = "https://www.materiel.net"
+    search_url = "https://www.materiel.net/recherche/"
+    selectors = {
+        'products': '.c-products-list__item',
+        'name': '.c-product__title',
+        'brand': '.c-product__title',  # Assuming brand is also inside title element
+        'price': '.o-product__price',
+        'description': '.c-product__description',
+        'url_product': '.c-product__link',
+        'url_image': '.c-product__thumb > a > img.img-fluid'
+    }
+
+    # Create an instance of the Site class for Materiel.net
+    materiel_net_site = Site('materiel', base_url, search_url, selectors)
+
+    # Get product information
+    product = "ecouteurs"
+    materiel_net_site.write_data(product_name=product)
