612e6960407ca6e3dbf264c5cec42e73d00d7157
DAMMAK26 <iyeddammakpro@gmail.com>
2024-04-16 03:35:58 +0200

    damma

--- analyses/main.py
+++ analyses/main.py
@@ -4,12 +4,17 @@ import numpy as np
 from product import Product
 from demand import Demand
 from cart import Cart 
- 
+
+
+def send_requests(demand):
+     
+    return csv_files 
+
 def fill_delivery(demands):
     delivery=[] #list of products
     for demand in demands: 
         cart=Cart(demand)
-        csv_files=fill_csv(demand)
+        csv_files=send_requests(demand)
         cart.set_products(csv_files)
         sorted_products=cart.fill_the_demand()
         delivery.append(sorted_products[0])
