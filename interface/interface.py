d50ea19ce2810dd55cac859c4e9411dd46bc77a6
000Bo <fbonnisseau.04@gmail.com>
2024-05-02 14:56:25 +0200

    création de l'interface en POO

---
+++ interface/interface.py
@@ -0,0 +1,324 @@
+from tkinter import Tk, Canvas, Entry, Button, PhotoImage, Checkbutton, Scrollbar
+from pathlib import Path
+
+class Block:
+    def __init__(self, canvas, window, current_y_position, add_block, images):
+        self.canvas = canvas
+        self.window = window
+        self.current_y_position = current_y_position
+        self.add_block = add_block
+        self.previous_add_button = None
+        self.block_images = []
+        self.images = images
+
+    def add(self):
+        #supprimer le bouton add_item du bloc précédent si c'est au moins le deuxieme bloc
+        if self.previous_add_button:
+            self.previous_add_button.destroy()
+        
+        # créer les éléments d'un bloc
+        self.canvas.create_rectangle(
+            81.0,
+            self.current_y_position + 217.9999999999999,
+            556.0,
+            self.current_y_position + 219.0,
+            fill="#1EBA65",
+            outline=""
+        )
+        
+        add_item_button = Button(
+            self.window,
+            image=self.images['button_image_1'],
+            borderwidth=0,
+            highlightthickness=0,
+            command=self.add_block,
+            relief="flat"
+        )
+        add_item_button.place(
+            x=309.0,
+            y=self.current_y_position + 240.0,
+            width=28.0,
+            height=28.0
+        )
+        self.previous_add_button = add_item_button
+        
+        entry_image_1 = PhotoImage(
+            file=relative_to_assets("entry_1.png")
+        )
+        entry_bg_1 = self.canvas.create_image(
+            219.5,
+            self.current_y_position + 186.0,
+            image=entry_image_1
+        )
+        product_input = Entry(
+            self.window,
+            bd=0,
+            bg="#D3D3D3",
+            fg="#000716",
+            highlightthickness=0
+        )
+        product_input.place(
+            x=120.0,
+            y=self.current_y_position + 172.0,
+            width=199.0,
+            height=26.0
+        )
+        self.block_images.append((entry_image_1,))
+        
+        entry_image_2 = PhotoImage(
+            file=relative_to_assets("entry_2.png")
+        )
+        entry_bg_2 = self.canvas.create_image(
+            411.0,
+            self.current_y_position + 186.0,
+            image=entry_image_2
+        )
+        brand_input = Entry(
+            self.window,
+            bd=0,
+            bg="#D3D3D3",
+            fg="#000716",
+            highlightthickness=0
+        )
+        brand_input.place(
+            x=356.0,
+            y=self.current_y_position + 172.0,
+            width=110.0,
+            height=26.0
+        )
+        
+        self.block_images.append((entry_image_2,))
+        
+        self.canvas.create_text(
+            114.0,
+            self.current_y_position + 152.0,
+            anchor="nw",
+            text="Nom du produit *",
+            fill="#000000",
+            font=("Inter Medium", 14 * -1)
+        )
+        
+        self.canvas.create_text(
+            350.0,
+            self.current_y_position + 152.0,
+            anchor="nw",
+            text="Marque",
+            fill="#000000",
+            font=("Inter Medium", 14 * -1)
+        )
+
+        button_image_2 = PhotoImage(
+            file=relative_to_assets("button_2.png")
+        )
+        
+        self.block_images.append((button_image_2,))
+        
+        self.current_y_position += 100
+
+#création de la classe Shopping app qui va contenir l'interface graphique
+class ShoppingApp:
+    def __init__(self):
+        self.window = Tk()
+        self.window.geometry("973x605")
+        self.window.configure(bg="#FFFFFF")
+
+        self.canvas = Canvas(
+            self.window,
+            bg="#FFFFFF",
+            height=605,
+            width=973,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge"
+        )
+        self.canvas.place(x=0, y=0)
+
+        # Add text and buttons
+        self.canvas.create_rectangle(
+            0.0,
+            0.0,
+            973.0,
+            73.0,
+            fill="#1EBA65",
+            outline=""
+        )
+        self.canvas.create_text(
+            266.0,
+            27.0,
+            anchor="nw",
+            text="Ecaddy - outil de shopping pour le matériel informatique",
+            fill="#FFFFFF",
+            font=("Inter Bold", 16 * -1)
+        )
+        self.canvas.create_text(
+            31.0,
+            116.0,
+            anchor="nw",
+            text="CHOISIR DES PRODUITS",
+            fill="#000000",
+            font=("Inter Bold", 16 * -1)
+        )
+        self.canvas.create_text(
+            770.0,
+            177.0,
+            anchor="nw",
+            text="Alternate",
+            fill="#000000",
+            font=("Inter Bold", 16 * -1)
+        )
+        self.canvas.create_text(
+            667.0,
+            116.0,
+            anchor="nw",
+            text="CHOISIR DES MAGASINS",
+            fill="#000000",
+            font=("Inter Bold", 16 * -1)
+        )
+        self.canvas.create_text(
+            770.0,
+            225.0,
+            anchor="nw",
+            text="Boulanger",
+            fill="#000000",
+            font=("Inter Bold", 16 * -1)
+        )
+        self.canvas.create_text(
+            770.0,
+            273.0,
+            anchor="nw",
+            text="CyberTech",
+            fill="#000000",
+            font=("Inter Bold", 16 * -1)
+        )
+        self.canvas.create_text(
+            770.0,
+            321.0,
+            anchor="nw",
+            text="GrosBill",
+            fill="#000000",
+            font=("Inter Bold", 16 * -1)
+        )
+        self.canvas.create_text(
+            770.0,
+            369.0,
+            anchor="nw",
+            text="Materiel.net",
+            fill="#000000",
+            font=("Inter Bold", 16 * -1)
+        )
+
+        self.checkbox_boulanger = Checkbutton(
+            self.window,
+            onvalue=True,
+            offvalue=False,
+            height=2,
+            width=2,
+            bg="#fff",
+            activebackground="#fff",
+            highlightthickness=0
+        )
+        self.checkbox_boulanger.place(x=715.0, y=220.0)
+
+        self.checkbox_cybertech = Checkbutton(
+            self.window,
+            onvalue=True,
+            offvalue=False,
+            height=2,
+            width=2,
+            bg="#fff",
+            activebackground="#fff",
+            highlightthickness=0
+        )
+        self.checkbox_cybertech.place(x=715.0, y=268.0)
+
+        self.checkbox_grosbill = Checkbutton(
+            self.window,
+            onvalue=True,
+            offvalue=False,
+            height=2,
+            width=2,
+            bg="#fff",
+            activebackground="#fff",
+            highlightthickness=0
+        )
+        self.checkbox_grosbill.place(x=715.0, y=316.0)
+
+        self.checkbox_materiel = Checkbutton(
+            self.window,
+            onvalue=True,
+            offvalue=False,
+            height=2,
+            width=2,
+            bg="#fff",
+            activebackground="#fff",
+            highlightthickness=0
+        )
+        self.checkbox_materiel.place(x=715.0, y=364.0)
+
+        self.checkbox_alternate = Checkbutton(
+            self.window,
+            onvalue=True,
+            offvalue=False,
+            height=2,
+            width=2,
+            bg="#fff",
+            activebackground="#fff",
+            highlightthickness=0
+        )
+        self.checkbox_alternate.place(x=715.0, y=172.0)
+
+        self.canvas.create_rectangle(
+            641.0,
+            66.0,
+            647.0000000000001,
+            650.0216064453125,
+            fill="#1EBA65",
+            outline=""
+        )
+
+        self.current_y_position = 0
+        self.images = {
+            'button_image_1': PhotoImage(file=relative_to_assets("button_1.png")),
+            'entry_image_1': PhotoImage(file=relative_to_assets("entry_1.png")),
+            'entry_image_2': PhotoImage(file=relative_to_assets("entry_2.png")),
+            'button_image_2': PhotoImage(file=relative_to_assets("button_2.png"))
+        }
+        self.block = Block(self.canvas, self.window, self.current_y_position, self.add_block, self.images)
+        
+        self.add_block()
+
+        button_image_5 = PhotoImage(
+            file=relative_to_assets("button_5.png")
+        )
+        self.comparer = Button(
+            self.window,
+            image=button_image_5,
+            borderwidth=0,
+            highlightthickness=0,
+            command=self.compare,
+            relief="flat"
+        )
+        self.comparer.place(
+            x=670.0,
+            y=509.0,
+            width=174.0,
+            height=64.0
+        )
+
+        self.window.resizable(False, False)
+        self.window.mainloop()
+
+    def add_block(self):
+        self.block.add()
+
+    def compare(self):
+        pass
+
+def relative_to_assets(path: str) -> Path:
+    OUTPUT_PATH = Path(__file__).parent
+    ASSETS_FOLDER = "assets/frame0"
+    ASSETS_PATH = OUTPUT_PATH / ASSETS_FOLDER
+    return ASSETS_PATH / Path(path)
+
+if __name__ == "__main__":
+    app = ShoppingApp()
